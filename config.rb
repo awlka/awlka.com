418f4a0f4bab70f437d166c90e8b1535fa0bdbdf
Lucas Churchill <lucas@agtlucas.com>
2014-06-04 10:54:53 -0300

    Adding config.rb

---
+++ config.rb
@@ -0,0 +1,16 @@
+preferred_syntax = :scss
+
+http_path = ''
+
+css_dir = 'public/css'
+sass_dir = 'public/_sass'
+images_dir = 'public/img'
+javascripts_dir = 'public/js'
+
+relative_assets = false
+line_comments = false
+output_style = :compressed
+
+asset_cache_buster :none
+
+Sass::Script::Number.precision = 8
